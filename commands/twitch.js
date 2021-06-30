9b052a3111b85ab25f72c19dd5fd4cb68ee3315e
Stijn <stijn@ardusit.nl>
2021-06-30 15:19:55 +0200

    Version 6.30.2021.3
    Added twitch command an gitignore

---
+++ commands/twitch.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'twitch',
+    description: 'Sends Twitch link <3',
+    execute(message, args){
+        message.channel.send('https://www.twitch.tv/st1n0_/');
+    }
+}
