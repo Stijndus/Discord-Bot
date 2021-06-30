9e4f564a3b275c8a306f4ce99eab79460a90c728
Stijn <stijn@ardusit.nl>
2021-06-30 15:15:24 +0200

    Version 6.30.2021.2
    Added Event/Command Handler and a config file for your token and prefix

--- bot.js
+++ bot.js
@@ -1,9 +1,32 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 
+
+const {token,prefix} = require('./config.json')
 const client = new Discord.Client();
 
+client.commands =  new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
 client.on('ready', () =>{
-    console.log(`Logged in as ${client.user.tag}!`)
+    console.log(`Logged in as ${client.user.tag}!`);
 })
 
-client.login('ODU5NzEyMzIwNzQxMTc5NDIx.YNwrfQ.s_YUxYE0NAEeYxx083K28yu1EJ0');
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ + /);
+    const command = args.shift().toLowerCase();
+
+    if(command === "ping"){
+        client.commands.get('ping').execute(message, args);
+    }
+})
+
+client.login(token);
